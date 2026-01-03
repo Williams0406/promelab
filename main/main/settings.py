d5985e02cd86e324c92365d87ca129a2032f6d5a
Williams Rodriguez <williams.rc04@gmail.com>
2026-01-03 13:07:04 -0500

    fix cloudinary

--- main/main/settings.py
+++ main/main/settings.py
@@ -168,3 +168,9 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
