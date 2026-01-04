eccaaf340649d36dd3e3591db236725714bedb49
Williams Rodriguez <williams.rc04@gmail.com>
2026-01-04 08:56:32 -0500

    prueba frontend 2

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,5 +1,21 @@
 import HomePage from "@/components/public/HomePage";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import WhatsAppButton from "@/components/common/WhatsAppButton";
 
 export default function Page() {
-  return <HomePage />;
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+
+      <main className="flex-1 w-full">
+        <HomePage />
+      </main>
+
+      <Footer />
+
+      {/* Botón flotante WhatsApp */}
+      <WhatsAppButton />
+    </div>
+  );
 }
